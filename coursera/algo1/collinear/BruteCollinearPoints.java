5a07df281cd87d98afa78ada8b17bbb4d760ba81
engineerchuan <engineerchuan@gmail.com>
2017-07-15 02:27:45 -0400

    added Brute and Fast

--- coursera/algo1/collinear/BruteCollinearPoints.java
+++ coursera/algo1/collinear/BruteCollinearPoints.java
@@ -1,39 +1,65 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+//import edu.princeton.cs.algs4.StdOut;
 
 public class BruteCollinearPoints {
     
     private final List<LineSegment> finalsegments;
+    private final Point[] points;
     
-    public BruteCollinearPoints(Point[] points) {
-        finalsegments = new ArrayList<LineSegment>();
-        if (points.length < 4) {
-            throw new IllegalArgumentException(" # points must be greater than 4");
+    private void checkArguments1(Point[] points) {
+        if (points == null) {
+          throw new IllegalArgumentException("points is null");
         }
         for (int i = 0; i < points.length; ++i) {
-            for (int j = i+1; j < points.length; ++j) {
-                for (int k = j+1; k < points.length; ++k) {
-                    for (int l = k+1; l < points.length; ++l) {
-                        if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[k]) &&
-                            points[i].slopeTo(points[j]) == points[i].slopeTo(points[l])) {
-                            
-                            // we have a match!
-                            Point[] segmentPoints = new Point[4];
-                            segmentPoints[0] = points[i];
-                            segmentPoints[1] = points[j];
-                            segmentPoints[2] = points[k];
-                            segmentPoints[3] = points[l];
-                            
-                            Arrays.sort(segmentPoints);
-                            LineSegment ls = new LineSegment(segmentPoints[0], segmentPoints[3]);
-                            finalsegments.add(ls);
+            if (points[i] == null) {
+              throw new IllegalArgumentException("a point in here is null");
+            }
+        }
+    
+    }
+    private void checkArguments2(Point[] points) {
+        Arrays.sort(points);
+        for (int i = 0; i < points.length-1 ; ++i) {
+            if (points[i].compareTo(points[i+1]) == 0) {
+              throw new IllegalArgumentException("repeated point");
+            }
+        }
+    }
+    public BruteCollinearPoints(Point[] points2) {
+        
+        checkArguments1(points2);
+        
+        // copy over
+        points = new Point[points2.length];
+        for (int i = 0; i < points2.length; ++i) {
+            points[i] = points2[i];
+        }
+        checkArguments2(points);
+        Arrays.sort(points);
+        finalsegments = new ArrayList<LineSegment>();
+        if (points.length < 4) {
+            // there will be no matches
+        }
+        else {
+            for (int i = 0; i < points.length; ++i) {
+                for (int j = i+1; j < points.length; ++j) {
+                    for (int k = j+1; k < points.length; ++k) {
+                        for (int l = k+1; l < points.length; ++l) {
+                            if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[k]) &&
+                                points[i].slopeTo(points[j]) == points[i].slopeTo(points[l])) {
+                                
+                                // assumes points are already sorted
+                                LineSegment ls = new LineSegment(points[i], points[l]);
+                                //StdOut.println("pushing " + points[i] + points[l]);
+                                finalsegments.add(ls);
+                            }
                         }
                     }
                 }
             }
-        }
-        
+        }        
     }    // finds all line segments containing 4 points
     public int numberOfSegments() {
         return finalsegments.size();
--- coursera/algo1/collinear/FastCollinearPoints.java
+++ coursera/algo1/collinear/FastCollinearPoints.java
@@ -1,39 +1,104 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+// import edu.princeton.cs.algs4.StdOut;
 
 public class FastCollinearPoints {
     
+    private final Point[] points;
     private final List<LineSegment> finalsegments;
-    public FastCollinearPoints(Point[] points) {
+    
+    private void checkArguments1(Point[] points) {
+        if (points == null) {
+          throw new IllegalArgumentException("points is null");
+        }
+        for (int i = 0; i < points.length; ++i) {
+            if (points[i] == null) {
+              throw new IllegalArgumentException("a point in here is null");
+            }
+        }
+    
+    }
+    private void checkArguments2(Point[] points) {
+        Arrays.sort(points);
+        for (int i = 0; i < points.length-1 ; ++i) {
+            if (points[i].compareTo(points[i+1]) == 0) {
+              throw new IllegalArgumentException("repeated point");
+            }
+        }
+    }
+    public FastCollinearPoints(Point[] points2) {
+        
+        checkArguments1(points2);
+        
+        // copy over
+        points = new Point[points2.length];
+        for (int i = 0; i < points2.length; ++i) {
+            points[i] = points2[i];
+        }
+        checkArguments2(points);
+        Arrays.sort(points);
         
         finalsegments = new ArrayList<LineSegment>();
+        
         if (points.length < 4) {
-            throw new IllegalArgumentException(" # points must be greater than 4");
+            // pass, cannot possibly have a match
         }
-        for (int i = 0; i < points.length - 3 + 1; ++i) {
-            Arrays.sort(points); // sort back to original order
-            Arrays.sort(points, i+1, points.length, points[i].slopeOrder());
+        else {
+            // StdOut.println("points length" + points.length);
             
-            int nMatch = 0;
-            for (int j = i + 1; j < points.length; ++j) {
-                if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[j+1])) {
-                    nMatch += 1;
-                }
-                else {
-                    if (nMatch >= 2) {
-                        // push a set of lines onto
-                        int nPoints = nMatch + 1;
-                        Point[] segmentPoints = new Point[nPoints];
-                        for (int k = 0; k < nPoints; ++k) {
-                            segmentPoints[k] = points[j - nMatch + k];
+            for (int i = 0; i < points.length; ++i) {
+                Arrays.sort(points); // sort back to original order
+                Point pt = points[i];
+                // StdOut.println("point i = " + pt);
+                
+                Arrays.sort(points, pt.slopeOrder());
+                
+                List<Integer> breaks = new ArrayList<Integer>(); // start / end
+                int lastStart = 0;
+                for (int j = 0; j < points.length-1; ++j) {
+                    if ((pt.slopeTo(points[j]) != pt.slopeTo(points[j+1]))) {
+                        if ((j + 1 - lastStart) >= 3) {
+                            breaks.add(lastStart);
+                            breaks.add(j+1);
+                            
+                            // StdOut.println("pushing a break " + lastStart + ", " + (j+1));
+                            lastStart = j+1;
                         }
-                        Arrays.sort(segmentPoints);
-                        LineSegment ls = new LineSegment(points[i], segmentPoints[nPoints-1]);
-                        finalsegments.add(ls);
-                        
+                        else {
+                            lastStart = j+1;
+                        }
                     }
                 }
+                if ((points.length - lastStart) >= 3) {
+                    breaks.add(lastStart);
+                    breaks.add(points.length);
+                    // StdOut.println("pushing a break at end " + lastStart + ", " + points.length);
+                }
+                
+                // do the breaks
+                for (int k = 0; k < breaks.size(); k += 2) {
+                    int start = breaks.get(k);
+                    int end = breaks.get(k+1);
+                    Arrays.sort(points, start, end);
+                    LineSegment ls;
+                    
+                    if (pt.compareTo(points[start]) < 0) {
+                        ls = new LineSegment(pt, points[end-1]);
+                        
+                        finalsegments.add(ls);
+                        // StdOut.println("push " + ls);
+                        
+                    }
+                    
+                    // else if (pt.compareTo(points[end-1]) > 0) {
+                    //  ls = new LineSegment(points[start], pt);
+                    // } else {
+                    //  ls = new LineSegment(points[start], points[end-1]);
+                    // }
+                    
+                }
+                
             }
         }
     }
